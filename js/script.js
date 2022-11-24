2141d4ff8d20fb9d597a7901a4a0d0408405d2e6
viliamnovicky <viliamnovicky@gmail.com>
2022-11-24 19:54:30 +0100

    certificates // modal

--- js/script.js
+++ js/script.js
@@ -22,3 +22,35 @@ copyNumberBTN.addEventListener("click", function() {
         copiedNumber.classList.add("hidden")
     }, 700)
 })
+
+// Show Modal 
+const educationSection = document.querySelector(".section__education")
+
+const modal = document.querySelector(".modal")
+const modalImg = document.querySelector(".modal__img")
+const closeModalBTN = document.querySelector(".btn__close")
+
+const showModal = function() {
+    modal.classList.remove("hidden")
+}
+
+const hideModal = function() {
+    modal.classList.add("hidden")
+}
+
+educationSection.addEventListener("click", function(e) {
+    if (e.target.closest(".certificate")) {
+        const certificate = e.target.closest(".certificate")
+        console.log("certificate");
+        modal.classList.remove("hidden")
+        modalImg.src = `../img/${certificate.id}.jpg`
+    }
+})
+
+
+closeModalBTN.addEventListener("click", hideModal)
+modal.addEventListener("click", hideModal)
+
+
+
+
